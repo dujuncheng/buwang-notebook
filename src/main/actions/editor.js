bce072503b34f4604a60df3343f1275cb036fd67
juncheng.du <juncheng.du@beibei.com>
2019-01-16 11:41:40 +0800

    【fix bugs】 修复了打包出错的问题

--- src/main/actions/editor.js
+++ src/main/actions/editor.js
@@ -1,5 +1,3 @@
-import { dialog, ipcMain, BrowserWindow } from 'electron'
-var log = require('electron-log')
 // 和编辑器有关的
 export const edit = (win, type) => {
     win.webContents.send('AGANI::edit', { type })
